d7edc3b063438c3a7d4d883df542842ee16863f4
Stefano Speretta <s.speretta@tudelft.nl>
2019-07-21 23:42:13 +0200

    Added INA226, working example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <driverlib.h>
 #include "msp.h"
 #include "PQ9Bus.h"
+#include "DWire.h"
+#include "INA226.h"
 #include "DSerial.h"
 #define FCLOCK 48000000
 
+DWire i2c0(0);
+INA226 INAbus0(i2c0, 0x40);
+INA226 INAbus1(i2c0, 0x49);
 PQ9Bus uart(3, GPIO_PORT_P9, GPIO_PIN0);
 DSerial serial;
 
@@ -49,8 +54,18 @@ void main(void)
     MAP_CS_initClockSignal(CS_SMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_4);
     MAP_CS_initClockSignal(CS_HSMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_2);
 
+    // Initialize I2C master
+    i2c0.setFastMode();
+    i2c0.begin();
+
+    // initialize the shunt resistor
+    INAbus0.setShuntResistor(0.04);
+
+    // initialize the shunt resistor
+    INAbus1.setShuntResistor(0.04);
+
     serial.begin( );        //baud rate: 9600 bps
-    uart.begin(115200, 4);  //baud rate: 115200 bps
+    uart.begin(115200, 5);  //baud rate: 115200 bps
     uart.onReceive(&onReceive);
 
     for (int i = 0; i < BUFFER_LENGTH; i++)
@@ -60,6 +75,10 @@ void main(void)
 
     serial.println("Hello World");
 
+    unsigned short v = 0;
+    signed short i = 0;
+    unsigned short p = 0;
+
     while(true)
     {
         if (dataReceived)
@@ -67,6 +86,51 @@ void main(void)
             RxData[6] = 2;
             uart.transmit(RxData[2], RxData[0], &RxData[3], RxData[1]);
             dataReceived = false;
+
+            if (!INAbus0.getVoltage(v))
+            {
+                serial.print("Voltage: ");
+                serial.print(v, DEC);
+                serial.println(" mV");
+            }
+            else
+            {
+                serial.println("Error reading voltage bus 0");
+            }
+
+            if (!INAbus0.getCurrent(i))
+            {
+                serial.print("Current: ");
+                serial.print(i, DEC);
+                serial.println(" mA");
+            }
+            else
+            {
+                serial.println("Error reading current bus 0");
+            }
+
+            if (!INAbus0.getPower(p))
+            {
+                serial.print("Power: ");
+                serial.print(p, DEC);
+                serial.println(" mW");
+            }
+            else
+            {
+                serial.println("Error reading power bus 0");
+            }
+
+            if (!INAbus1.getVoltage(v))
+            {
+                serial.print("Voltage: ");
+                serial.print(v, DEC);
+                serial.println(" mV");
+            }
+            else
+            {
+                serial.println("Error reading voltage bus 1");
+            }
+            serial.println();
         }
     }
 }

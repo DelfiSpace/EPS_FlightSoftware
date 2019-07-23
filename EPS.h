5b4600e81746603b5aeb10f2dc1b8133b181959a
Stefano Speretta <s.speretta@tudelft.nl>
2019-07-23 23:36:07 +0200

    Using PQ9Frame instead un a char array to contain bus messages
    Setting also by default the project to be in Release mode

---
+++ EPS.h
@@ -0,0 +1,22 @@
+/*
+ * EPS.h
+ *
+ *  Created on: 23 Jul 2019
+ *      Author: stefanosperett
+ */
+
+#ifndef EPS_H_
+#define EPS_H_
+
+
+#include <driverlib.h>
+#include "msp.h"
+#include "PQ9Bus.h"
+#include "PQ9Frame.h"
+#include "DWire.h"
+#include "INA226.h"
+#include "DSerial.h"
+
+#define FCLOCK 48000000
+
+#endif /* EPS_H_ */
--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
-#include <driverlib.h>
-#include "msp.h"
-#include "PQ9Bus.h"
-#include "DWire.h"
-#include "INA226.h"
-#include "DSerial.h"
-#define FCLOCK 48000000
+
+#include "EPS.h"
 
 DWire i2c0(0);
 INA226 INAbus0(i2c0, 0x40);
@@ -16,16 +11,15 @@ DSerial serial;
 
 unsigned char Txdata[BUFFER_LENGTH];
 
-unsigned char RxData[261];
+
+PQ9Frame rxBuffer, txBuffer;
+
 uint8_t addr;
 volatile bool dataReceived = false;
 
-void onReceive( unsigned char *buffer)
+void onReceive( PQ9Frame &newFrame)
 {
-    for (int i = 0; i < buffer[1] + 5; i++)
-    {
-        RxData[i] = buffer[i];
-    }
+    newFrame.copy(rxBuffer);
     dataReceived = true;
 }
 
@@ -66,7 +60,7 @@ void main(void)
 
     serial.begin( );        //baud rate: 9600 bps
     uart.begin(115200, 5);  //baud rate: 115200 bps
-    uart.onReceive(&onReceive);
+    uart.setReceiveHandler(&onReceive);
 
     for (int i = 0; i < BUFFER_LENGTH; i++)
     {
@@ -83,8 +77,17 @@ void main(void)
     {
         if (dataReceived)
         {
-            RxData[6] = 2;
-            uart.transmit(RxData[2], RxData[0], &RxData[3], RxData[1]);
+            // copy the received buffer into the transmitter buffer
+            rxBuffer.copy(txBuffer);
+
+            // swap source and destination to respond
+            txBuffer.setDestination( rxBuffer.getSource() );
+            txBuffer.setSource( rxBuffer.getDestination() );
+
+            // transmit
+            uart.transmit( txBuffer );
+
+            // clear the flag, waiting for the next received command
             dataReceived = false;
 
             if (!INAbus0.getVoltage(v))

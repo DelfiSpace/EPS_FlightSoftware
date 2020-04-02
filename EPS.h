7400660c22d27bd41b489d335b7794f46dd16987
Casper <broekhuizen.casper@gmail.com>
2020-04-02 11:40:19 +0200

    Added HWMonitor and ADCManager

--- EPS.h
+++ EPS.h
@@ -36,6 +36,8 @@
 #include "PowerBusHandler.h"
 #include "PeriodicTaskNotifier.h"
 #include "Bootloader.h"
+#include "HWMonitor.h"
+#include "ADCManager.h"
 
 #define FCLOCK 48000000
 #define SCLOCK 32768
--- main.cpp
+++ main.cpp
@@ -38,6 +38,9 @@ TMP100 tempXm(SolarPanelsBus, 0x49);
 DSPI spi(3);
 MB85RS fram(spi, GPIO_PORT_P1, GPIO_PIN0 );
 
+// HardwareMonitor
+HWMonitor hwMonitor(&fram);
+
 // Bootloader
 Bootloader bootLoader = Bootloader(fram);
 
@@ -109,6 +112,7 @@ void acquireTelemetry(EPSTelemetryContainer *tc)
 
     // set uptime in telemetry
     tc->setUpTime(uptime);
+    tc->setMCUTemperature(hwMonitor.getMCUTemp());
 
     // measure the battery board
     tc->setBattStatus((!gasGauge.getVoltage(v)) &
@@ -213,6 +217,11 @@ void main(void)
     // - clock tree
     DelfiPQcore::initMCU();
 
+    // initialize the ADC
+    // - ADC14 and FPU Module
+    // - MEM0 for internal temperature measurements
+    ADCManager::initADC();
+
     // Initialize I2C masters
     I2Cinternal.setFastMode();
     I2Cinternal.begin();
@@ -260,6 +269,11 @@ void main(void)
     // - initialize the pins for the hardware watch-dog
     // - prepare the pin for power cycling the system
     reset.init();
+
+    // initialize HWMonitor readings
+    hwMonitor.readResetStatus();
+    hwMonitor.readCSStatus();
+
 
     // link the command handler to the PQ9 bus:
     // every time a new command is received, it will be forwarded to the command handler
